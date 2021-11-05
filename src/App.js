f617a24b301aa5bdeb3b3e09965190a683e3806f
Samuel Lau <samuel.cflau@gmail.com>
2021-11-06 02:05:22 +0800

    Component state, event handlers

--- src/App.js
+++ src/App.js
@@ -1,60 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Header = (props) => {
-  return <h1>{props.course}</h1>
-}
+const Display = ({ counter }) => <div>{counter}</div>
 
-const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
-      <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
-      <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part} {props.exercise}
-    </p>
-  )
-}
-
-const Total = (props) => (
-  <p>
-    Number of exercises{' '}
-    {props.parts[0].exercises +
-      props.parts[1].exercises +
-      props.parts[2].exercises}
-  </p>
-)
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-      }
-    ]
-  }
+  const [counter, setCounter] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
+  // const handleClick = () => {
+  //   console.log('clicked')
+  // }
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text='plus' />
+      <br />
+      <Button onClick={decreaseByOne} text='minus' />
+      <br />
+      <Button onClick={setToZero} text='zero' />
     </div>
   )
 }
