82b561c09ad2fb16cbf87283d93770d12d568ecc
Samuel Lau <samuel.cflau@gmail.com>
2021-11-09 00:17:45 +0800

    Ex1.6

--- src/App.js
+++ src/App.js
@@ -1,57 +1,36 @@
-import React from 'react'
-import Course from './components/Course'
+import React, { useState } from 'react'
+
+
 
 const App = () => {
-  const course = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1,
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2,
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3,
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4,
-        },
-      ],
-    },
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1,
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2,
-        },
-      ],
-    },
-  ]
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleClickGood = () => {
+    setGood(good + 1)
+  }
+  const handleClickNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+  const handleClickBad = () => {
+    setBad(bad + 1)
+  }
 
   return (
-    <div>
-      <h1>Web development curriculum</h1>
-      {course.map(course => <Course course={course} key={course.id}/>)}
-    </div>
+    <>
+      <div>
+        <h1>give feedback</h1>
+        <button onClick={handleClickGood}>good</button>
+        <button onClick={handleClickNeutral}>neutral</button>
+        <button onClick={handleClickBad}>bad</button>
+        <h1>statistics</h1>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+      </div>
+    </>
   )
 }
 
