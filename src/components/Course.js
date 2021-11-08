0d78e42d069578bfb322277a78e17552f48f63c4
Samuel Lau <samuel.cflau@gmail.com>
2021-11-08 14:46:27 +0800

    Ex2.4: expand from a single object to an array with multiple objects

--- src/components/Course.js
+++ src/components/Course.js
@@ -2,25 +2,24 @@ import React from 'react'
 
 const Course = ({ course }) => {
   return (
-    <div>
-      <Header course={course.name} />
+    <>
+      <Header header={course.name} />
       <Content parts={course.parts} />
       <Total parts={course.parts} />
-    </div>
+    </>
   )
 }
 
-const Header = ({ course }) => {
-  // console.log(`Header: ${JSON.stringify(props)}`)
-  return <h1>{course.course}</h1>
+const Header = ({ header }) => {
+  // console.log(course);
+  return <h1>{header}</h1>
 }
 
-const Content = (props) => {
-  // console.log(`Content: ${JSON.stringify(props)}`)
-  // console.log(props.parts)
+const Content = ({parts}) => {
+  console.log("Parts", parts)
   return (
     <div>
-      {props.parts.map((part) => {
+      {parts.map((part) => {
         return <Part name={part.name} exercise={part.exercises} key={part.id} />
       })}
       {/* <Part name={props.parts[0].name} exercise={props.parts[0].exercises} />
@@ -30,32 +29,31 @@ const Content = (props) => {
   )
 }
 
-const Part = (props) => {
-  // console.log(`Part: ${JSON.stringify(props)}`)
-  // console.log(props.name);
+const Part = ({name, exercise}) => {
+  console.log("Part: ", name, exercise);
   return (
     <p>
-      {props.name} {props.exercise}
+      {name} {exercise}
     </p>
   )
 }
 
-const Total = (props) => {
+const Total = ({parts}) => {
   // console.log(`Total: ${props}`)
-  console.log('Array of "props.parts": ', props.parts)
+  // console.log('Array of "props.parts": ', props.parts)
 
-  const parts = props.parts.map((course) => course.exercises)
-  console.log('Array of "parts":', parts)
+  const exercise = parts.map((course) => course.exercises)
+  // console.log('Array of "exercise":', exercise)
 
-  const total = parts.reduce((previousValue, currentValue) => {
+  const total = exercise.reduce((previousValue, currentValue) => {
     return previousValue + currentValue
   })
 
-  console.log('total: ', total)
+  // console.log('total: ', total)
 
   return (
     <p>
-      Number of exercises {total}
+      <strong>total of {total} exercises</strong>
       {/* {props.parts[0].exercises +
         props.parts[1].exercises +
         props.parts[2].exercises} */}
