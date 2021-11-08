04e57705c688be54ac071000be36d0347311d913
Samuel Lau <samuel.cflau@gmail.com>
2021-11-08 12:15:26 +0800

    Ex2.2-2.3: map & reduce

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const Course = ({ course }) => {
   )
 }
 
-const Header = ({course}) => {
+const Header = ({ course }) => {
   // console.log(`Header: ${JSON.stringify(props)}`)
   return <h1>{course.course}</h1>
 }
@@ -21,17 +21,11 @@ const Content = (props) => {
   return (
     <div>
       {props.parts.map((part) => {
-        return (
-          <Part
-            name={part.name}
-            exercise={part.exercises}
-            key={part.id}
-          />
-        )
+        return <Part name={part.name} exercise={part.exercises} key={part.id} />
       })}
-      <Part name={props.parts[0].name} exercise={props.parts[0].exercises} />
+      {/* <Part name={props.parts[0].name} exercise={props.parts[0].exercises} />
       <Part name={props.parts[1].name} exercise={props.parts[1].exercises} />
-      <Part name={props.parts[2].name} exercise={props.parts[2].exercises} />
+      <Part name={props.parts[2].name} exercise={props.parts[2].exercises} /> */}
     </div>
   )
 }
@@ -48,12 +42,23 @@ const Part = (props) => {
 
 const Total = (props) => {
   // console.log(`Total: ${props}`)
+  console.log('Array of "props.parts": ', props.parts)
+
+  const parts = props.parts.map((course) => course.exercises)
+  console.log('Array of "parts":', parts)
+
+  const total = parts.reduce((previousValue, currentValue) => {
+    return previousValue + currentValue
+  })
+  
+  console.log('total: ', total)
+
   return (
     <p>
-      Number of exercises{' '}
-      {props.parts[0].exercises +
+      Number of exercises {total}
+      {/* {props.parts[0].exercises +
         props.parts[1].exercises +
-        props.parts[2].exercises}
+        props.parts[2].exercises} */}
     </p>
   )
 }
@@ -77,10 +82,15 @@ const App = () => {
         exercises: 14,
         id: 3,
       },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4,
+      },
     ],
   }
 
-  return <Course course={course}/>
+  return <Course course={course} />
 }
 
 export default App
