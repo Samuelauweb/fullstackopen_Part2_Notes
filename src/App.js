fc73e07e18f2736357c08c6a368e22ae0b6cd0c1
Samuel Lau <samuel.cflau@gmail.com>
2021-11-10 15:41:10 +0800

    Ex2.6-2.7

--- src/App.js
+++ src/App.js
@@ -1,50 +1,50 @@
 import React, { useState } from 'react'
-import Note from './components/Note'
+import Person from './components/Person'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  // console.log(props.notes);
-  const [newNote, setNewNote] = useState('new note...')
-  // console.log(newNote);
-  const [showAll, setShowAll] = useState(true)
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
 
-  const notesToShow = showAll ? notes : notes.filter(note => note.important === true)
-
-  const addNote = (event) => {
+  const addPerson = (event) => {
     event.preventDefault()
-    // console.log('button clicked: ', event.target);
-    const noteObject = {
-      id: notes.length + 1,
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() < 0.5,
+    // console.log(event.target);
+    const personObject = {
+      name: newName,
+      id: Math.floor(Math.random() * 101),
     }
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+
+    if (persons.filter(person => person.name === personObject.name).length > 0){
+      console.log(persons.filter((person) => person.name === personObject.name))
+      alert(`${newName} is already added to phonebook`)
+    } else {
+
+      setPersons(persons.concat(personObject))
+      setNewName('')
+    }
   }
 
-  const handleNoteChange = (event) => {
-    console.log('event.target.value: ', event.target.value)
-    setNewNote(event.target.value)
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
   }
 
   return (
     <div>
-      <h1>Notes</h1>
-      <div>
-        <button onClick={() => {setShowAll(!showAll)}}>
-          show {showAll ? 'important' : 'all'}
-        </button>
-      </div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type='submit'>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>debug: {newName}</div>
       <ul>
-        {notesToShow.map((note) => (
-          <Note note={note} key={note.id} />
+        {persons.map((person) => (
+          <Person person={person} key={person.id} />
         ))}
       </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
-        <button type='submit'>save</button>
-      </form>
     </div>
   )
 }
